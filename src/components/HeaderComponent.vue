a84c57232aad448737098a81ca4224772faa397c
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-11-24 17:53:46 -0600

    falta solucionar que no se encuantra icon en header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <icon-component />
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+import { IconComponent } from "@/components/IconComponent.vue";
+export default {
+  name: "Header",
+  components: { IconComponent },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+//importa la libreria
 import Vue from "vue";
+//importa el componente
 import App from "./App.vue";
-
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+    render: (h) => h(App),
 }).$mount("#app");
