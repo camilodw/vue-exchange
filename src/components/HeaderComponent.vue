5b2bb8c340c4169c8a0f379e60024ce4f152d211
Camilo Oyarzo <camilo.oyarzodw@gmail.com>
2021-11-25 18:19:33 -0600

    se agrega ruta para about

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -19,7 +19,7 @@
             <router-link to="/" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <router-link to="/about" class="nav-link">About</router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Pricing</a>
